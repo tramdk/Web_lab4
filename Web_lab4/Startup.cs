87938248dd707a847b9912753dd539631152a38d
tramdk <tramdk1997@gmail.com>
2019-03-26 09:36:56 +0700

    hello

---
+++ Web_lab4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_lab4.Startup))]
+namespace Web_lab4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
